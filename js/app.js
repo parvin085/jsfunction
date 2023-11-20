ffba2849d774a7752c3ed499a2dbee25812b0ec9
parvin085 <gahramanlipervin6@gmail.com>
2023-11-20 09:46:43 +0400

    js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function openmeny()
+{
+    // document.getElementsByClassName("demo1")[0].style.right = "0px"
+    // document.getElementsByClassName("demo1")[0].style.right = "-300px"
+    let a = document.getElementsByClassName("demo1")[0]
+    b = document.getElementsByClassName("demo")[0]
+    a.classList.toggle("seffaf")
+    b.classList.toggle("red")
+  
+}
